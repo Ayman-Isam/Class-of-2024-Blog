5ec8289a23f702adafdc92825561ae29d4972ff3
Ayman Isam <aymanisam26@gmail.com>
2021-12-28 19:33:02 +0200

    Created a rich text editor for the blog

--- blog/models.py
+++ blog/models.py
@@ -2,10 +2,11 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from tinymce.models import HTMLField
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    content = models.TextField()
+    content = HTMLField(blank=True, null=True)
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     likes = models.ManyToManyField(User, related_name='blog_posts')
