362ead8a6b86f9fb4d07f9d67cc28ec679b7da2a
Ayman Isam <aymanisam26@gmail.com>
2022-01-17 07:35:29 +0200

    Removed image resizing

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,6 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 from tinymce.models import HTMLField
-from django.utils.text import slugify
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
